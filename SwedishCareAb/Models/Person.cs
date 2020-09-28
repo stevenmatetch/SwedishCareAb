3194a99f97c351647f166cccea65dc7b39c14b42
steven <steven@DESKTOP-57NOQ64.SCS.LAN>
2020-09-28 13:46:18 +0200

    Add project files.

---
+++ SwedishCareAb/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwedishCareAb.Models
+{
+   public class Person
+    {
+       public int ID { get; set; }
+       public string PersonalIdentityNumber { get; set; }
+        public int Namn { get; set; }
+       public int FirstName  { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+
+
+    }
+}
